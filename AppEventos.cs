f54b2a1b012bdf854a1f7427bca1a58f428aa219
Dual-Ricardo-Uribia <99446565+Dual-Ricardo-Uribia@users.noreply.github.com>
2022-03-17 11:57:31 -0600

    Primera subida de código del ejercicio APP EVENTOS
    C# 
    Curso de Buenas Practicas PHP

---
+++ AppEventos.cs
@@ -0,0 +1,44 @@
+﻿using static ConsoleApp1.AppEventos.DesplegadorEventos;
+
+namespace ConsoleApp1
+{
+    internal class AppEventos
+    {
+        private IRecuperadorRegistros recuperador;
+        private IDesplegadorEventos desplegador;
+
+        public AppEventos(IRecuperadorRegistros recuperador, IDesplegadorEventos desplegador, object procesador)
+        {
+            this.recuperador = recuperador;
+            this.desplegador = desplegador;
+        }
+
+        public void Ejecutar()
+        {
+
+        }
+
+        public interface IDesplegadorEventos
+        {
+            void DesplegadorEventos(Evento EventoPorDesplegar);
+        }
+
+        public class DesplegadorEventos : IDesplegadorEventos
+        {
+            public DesplegadorEventos()
+            {
+
+            }
+
+            void IDesplegadorEventos.DesplegadorEventos(Evento EventoPorDesplegar)
+            {
+                throw new System.NotImplementedException();
+            }
+
+            public class Evento
+            {
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using ConsoleApp1;
+using ConsoleApp1.Recursos.Implementaciones;
+using ConsoleApp1.Recursos.RecuperarInformacion;
+using static ConsoleApp1.AppEventos;
+
+namespace Eventos 
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var DirectorioArchivo = @"C:\Debug";
+            var NombreArchivo = "eventos.txt";
+            var Controlador = new ControladorArchivo();
+            IRecuperadorRegistros Recuperador = new RecuperadorRegistros(DirectorioArchivo, NombreArchivo, Controlador);
+            IDesplegadorEventos Desplegador = new DesplegadorEventos();
+            var Procesador = ProcesadorEvento();
+            AppEventos App = new AppEventos(Recuperador, Desplegador, Procesador);
+            App.Ejecutar();
+
+        }
+
+        private static object ProcesadorEvento()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
+
